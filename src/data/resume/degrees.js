d1f525eaa7384e5080a05ff9be4e3de79020fd63
Mona Lisa <M.Chan15@lse.ac.uk>
2021-06-25 16:00:11 +0100

    commit

--- src/data/resume/degrees.js
+++ src/data/resume/degrees.js
@@ -1,15 +1,15 @@
 const degrees = [
   {
-    school: 'Stanford University',
-    degree: 'M.S. Computational and Mathematical Engineering',
-    link: 'https://stanford.edu',
-    year: 2016,
+    school: 'Imperial University',
+    degree: 'MSc Artificial Intelligence',
+    link: 'https://www.imperial.ac.uk/study/pg/computing/artificial-intelligence/',
+    year: 2022,
   },
   {
-    school: 'University at Buffalo',
-    degree: 'B.S. Electrical Engineering, Computer Engineering',
-    link: 'https://buffalo.edu',
-    year: 2012,
+    school: 'London School of Economics and Political Science(LSE)',
+    degree: 'BSc in Econometrics and Mathematical Economics',
+    link: 'https://www.lse.ac.uk/study-at-lse/Undergraduate/Degree-programmes-2022/BSc-Econometrics-and-Mathematical-Economics',
+    year: 2021,
   },
 ];
 
