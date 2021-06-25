dc3f5b088c423f96f0cb87e1e9b64be7533e7626
Mona Lisa <M.Chan15@lse.ac.uk>
2021-06-25 18:48:04 +0100

    Update positions.js

--- src/data/resume/positions.js
+++ src/data/resume/positions.js
@@ -4,7 +4,35 @@ const positions = [
     position: 'Research Assistant',
     daterange: 'January 2021 - Present',
     points: [
-      'Data Analysis',
+      'RA to Ethan Ilzetzki, Associate Professor at LSE and Centre of Macroeconomics',
+      'Assisted with data analysis',
+      'Ran and wrote code for value iteration functions in Matlab and Python'
+    ],
+  },
+  {
+    company: 'London School of Economics and Political Science(LSE)',
+    position: 'Coach, Python for Economics Week',
+    daterange: 'Feb 2021 - Feb 2021',
+    points: [
+      'Provided guidance to LSE economics undergraduates in writing code for the Schelling model'
+    ],
+  },
+  {
+    company: 'LSE Student Union(LSESU) Data Science Soceity',
+    position: 'Head of Projects',
+    daterange: 'July 2020 - Apr 2021',
+    points: [
+      'Lead teams in completing end-to-end data science projects',
+      'Responsible for recruitment and monitoring of developers',
+      'Sought partnerships with external companies'
+    ],
+  },
+  {
+    company: 'Singapore Department of Statistics',
+    position: 'Intern',
+    daterange: 'July 2020 - Sep 2020',
+    points: [
+      'Contributed to literature on synthetic data by introducing a comparative metrics framework that comprehensively and systematically evaluates the quality of synthetic data'
     ],
   },
 ];
