c0f9506b0933911a7a0e801f1732752dde7edd19
mc821@ic.ac.uk <mc821@ic.ac.uk>
2022-11-27 19:28:59 +0800

    remove file

--- src/components/Projects/Abstract.js
+++ src/components/Projects/Abstract.js
@@ -1,27 +1,26 @@
-import React from 'react';
+// import React from 'react';
 
-class TodoApp extends React.Component {
-    constructor(props) {
-      super(props)
-      this.state = {
-          render: false
-      }
-      this.alertHi = this.alertHi.bind(this);
-    }
+// class TodoApp extends React.Component {
+//     constructor(props) {
+//       super(props)
+//       this.state = {
+//           render: false
+//       }
+//       this.alertHi = this.alertHi.bind(this);
+//     }
     
-    alertHi() {
-        this.setState({render: !this.state.render});
-    }
+//     alertHi() {
+//         this.setState({render: !this.state.render});
+//     }
        
-    render() {
-      return(
-        <div className="App">
-          <button onClick={this.alertHi}>Abstract</button>
-          {this.state.render && <h1>Extra text</h1>}
-        </div>
-      );
-    }
-  }
+//     render() {
+//       return(
+//         <div className="App">
+//           <button onClick={this.alertHi}>Abstract</button>
+//           {this.state.render && <h1>Extra text</h1>}
+//         </div>
+//       );
+//     }
+//   }
   
-  ReactDOM.render(<TodoApp />, document.querySelector("#app"))
-  
+//   ReactDOM.render(<TodoApp />, document.querySelector("#app"))
