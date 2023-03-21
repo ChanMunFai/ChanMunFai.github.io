7a87d598c3ca8b912e148e5020eb42f7a8edecb5
mc821@ic.ac.uk <mc821@ic.ac.uk>
2023-03-20 20:07:15 -0400

    Add in unique key for SearchList

--- src/components/Projects/SearchList.js
+++ src/components/Projects/SearchList.js
@@ -2,7 +2,9 @@ import React from 'react';
 import Card from './Card';
 
 function SearchList({ filteredCards }) {
-  const filtered = filteredCards.map(post =>  <Card post={post}/>); 
+  const filtered = filteredCards.map(post =>  
+    <Card key={post.id} post={post}/>
+    ); 
   return (
     <div className='cards'>
       {filtered}
