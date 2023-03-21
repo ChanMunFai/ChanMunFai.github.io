2c344fae99e1dec26ee6d4461965721f300bc4bb
mc821@ic.ac.uk <mc821@ic.ac.uk>
2023-03-20 20:08:45 -0400

    Refactor code due to upgrade in React Router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Suspense, lazy } from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Main from './layouts/Main'; // fallback for lazy pages
 import './static/css/main.scss'; // All of our styles
 
@@ -9,9 +9,6 @@ import BlogData from './data/blog/blog_data';
 
 const { PUBLIC_URL } = process.env;
 
-// Every route - we lazy load so that each page can be chunked
-// NOTE that some of these chunks are very small. We should optimize
-// which pages are lazy loaded in the future.
 const About = lazy(() => import('./pages/About'));
 const Contact = lazy(() => import('./pages/Contact'));
 const Index = lazy(() => import('./pages/Index'));
@@ -27,19 +24,19 @@ const BlogPost = lazy(() => import('./components/BlogPost/BlogPost'));
 const App = () => (
   <BrowserRouter basename={PUBLIC_URL}>
     <Suspense fallback={<Main />}>
-      <Switch>
-        <Route exact path="/" component={Index} />
-        <Route path="/about" component={About} />
-        <Route path="/projects" component={Projects} />
-        <Route path="/blog" component={Blog} />
-        <Route path="/resources" component={Resources} />
-        <Route path="/stats" component={Stats} />
-        <Route path="/contact" component={Contact} />
-        <Route path="/resume" component={Resume} />
-        <Route path="/projectsposts/:id" render={(props) => <ProjectPost data={ProjectData} {...props} />} />
-        <Route path="/blogposts/:id" render={(props) => <BlogPost data={BlogData} {...props} />} />
-        <Route component={NotFound} status={404} />
-      </Switch>
+      <Routes>
+        <Route exact path="/" element={<Index/>} />
+        <Route path="/about" element={<About/>} />
+        <Route path="/projects" element={<Projects/>} />
+        <Route path="/blog" element={<Blog/>} />
+        <Route path="/resources" element={<Resources/>} />
+        <Route path="/stats" element={<Stats/>} />
+        <Route path="/contact" element={<Contact/>} />
+        <Route path="/resume" element={<Resume/>} />
+        <Route path="/projectsposts/:id" element={<ProjectPost data={ProjectData}/>} />
+        <Route path="/blogposts/:id" element={<BlogPost data={BlogData}/>} />
+        <Route element={<NotFound/>} status={404} />
+      </Routes>
     </Suspense>
   </BrowserRouter>
 );
